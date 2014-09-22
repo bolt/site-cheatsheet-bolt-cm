30e0bd3cb691341a10ece6819d0c2e8b94ee5549
Bob den Otter <bob@twokings.nl>
2014-09-22 21:54:14 +0200

    Add dump function to twig.

--- index.php
+++ index.php
@@ -9,12 +9,18 @@ use Symfony\Component\Yaml\Parser;
 $yaml = new Parser();
 $cheatsheet = $yaml->parse(file_get_contents('cheatsheet.yml'));
 
-\Dumper::dump($cheatsheet);
-
 $loader = new Twig_Loader_Filesystem('./view');
 $twig = new Twig_Environment($loader, array(
 /*    'cache' => './cache', */
 ));
+
+
+$dumper = new Twig_SimpleFunction(
+    'dump', 
+    function ($var) { return \Dumper::dump($var, DUMPER_CAPTURE); }, 
+    array('is_safe' => array('html')
+));
+$twig->addFunction($dumper);
 
 echo $twig->render('index.twig', array(
     'title' => "Bolt Cheatsheet",
