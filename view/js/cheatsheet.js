201036e0b7c1076005346dbf8d34cc8917c0597b
Bob den Otter <bob@twokings.nl>
2014-09-28 12:15:06 +0200

    Some javascript added, working on layout.

--- view/js/cheatsheet.js
+++ view/js/cheatsheet.js
@@ -1,12 +1,37 @@
 
 jQuery(function($) { 
 
-	SyntaxHighlighter.defaults['auto-links'] = false;
-	SyntaxHighlighter.all();
+    // SyntaxHighlighter.defaults['auto-links'] = false;
+    // SyntaxHighlighter.all();
 
-    // Initialize the Fancybox shizzle, if present.
-    if(jQuery().fancybox) {
-        $('.fancybox, div.imageholder a').fancybox({ });
-    }
+    //    // Initialize the Fancybox shizzle, if present.
+    //    if(jQuery().fancybox) {
+    //        $('.fancybox, div.imageholder a').fancybox({ });
+    //    }
+
+    addAnchors();
+
+    var $container = $('#panels');
+    // init
+    $container.packery({
+          itemSelector: '.columns',
+          gutter: 0
+    });
 
 });
+
+
+
+
+    function addAnchors(){
+        //loop through all your headers
+        $.each($('h2'), function(index, value){
+            //append the text of your header to a list item in a div, linking to an anchor we will create on the next line
+            $('#box-anchors').append('<li><a href="#anchor-'+index+'">'+$(this).html()+'</a></li>');
+            //add an a tag to the header with a sequential name
+            var id = URLify($(this).html());
+            var anchor = ' <a href="' + id + '" class="anchor">&para;</a>';
+            $(this).replaceWith('<h2 id="' + id + '">'+$(this).html() + anchor +'</h2>');
+        });
+    }
+
