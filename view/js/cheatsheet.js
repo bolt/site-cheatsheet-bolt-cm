c63ed917b0a7c5fbd5cb89c3a37478a9b5f956f6
bobdenotter <bob@twokings.nl>
2014-10-07 18:32:11 +0200

    Highlight on click.

--- view/js/cheatsheet.js
+++ view/js/cheatsheet.js
@@ -10,4 +10,10 @@ jQuery(function($) {
 
     $(document).foundation();
 
+    $('a[href*="#"]').click(function(){
+        var panel = $($(this).attr("href")).parent().parent();
+        panel.animate({"background-color": "#FF6", "border-color": "#888"}).delay(100).animate({"background-color": "#FFF", "border-color": "#ccc"});
+        console.log('hoi', panel);
+    });
+
 });
