ba53011b4ab69423e07f3dfc91d8ced683ba31e5
Bob den Otter <bob@twokings.nl>
2014-11-24 19:49:10 +0100

    Rendered assets.

--- view/js/cheatsheet.js
+++ view/js/cheatsheet.js
@@ -2537,14 +2537,17 @@ jQuery(function($) {
           gutter: 0
     });
 
+
+    window.setTimeout(function() { $container.packery(); }, 2500);
+
     $(document).foundation();
 
     $('a[href*="#"]').click(function(){
         var panel = $($(this).attr("href")).parent().parent();
         panel.animate({"background-color": "#FF6", "border-color": "#888"}).delay(100).animate({"background-color": "#FFF", "border-color": "#ccc"});
-        console.log('hoi', panel);
     });
 
 });
+
 
 //# sourceMappingURL=cheatsheet.js.map
