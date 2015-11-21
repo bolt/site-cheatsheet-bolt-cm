83b5c03189b19899d7a6132167a5e49d6b4f3db4
Bob den Otter <bob@twokings.nl>
2015-11-21 19:18:44 +0100

    Updating dependencies

--- index.php
+++ index.php
@@ -2,39 +2,43 @@
 
 require_once './vendor/autoload.php';
 
-$version = "2.2.13";
-
 use Symfony\Component\Yaml\Parser;
+use Cocur\Slugify\Slugify;
+
+$version = "2.2.13";
 
 $yaml = new Parser();
 $cheatsheet = $yaml->parse(file_get_contents('cheatsheet.yml'));
 
 $loader = new Twig_Loader_Filesystem('./view');
 $twig = new Twig_Environment($loader, array(
-  'cache' => './cache', 
+  'cache' => './cache',
 ));
 
-// Add Dumper function to twig. 
+// Add Dumper function to twig.
 $dumper = new Twig_SimpleFunction(
-    'dump', 
-    function ($var) { return \Dumper::dump($var, DUMPER_CAPTURE); }, 
+    'dump',
+    function ($var) { return dump($var); },
     array('is_safe' => array('html')
 ));
 $twig->addFunction($dumper);
 
 
-// Add markdown to twig. 
+// Add markdown to twig.
 $markdown = new Twig_SimpleFilter(
-    'markdown', 
-    function ($content) { return \ParsedownExtra::instance()->text($content); }, 
+    'markdown',
+    function ($content) { return \ParsedownExtra::instance()->text($content); },
     array('is_safe' => array('html')
 ));
 $twig->addFilter($markdown);
 
-// Add slug filter to twig. 
+// Add slug filter to twig.
 $slug = new Twig_SimpleFilter(
-    'slug', 
-    function ($name) { return \URLify::filter($name); }, 
+    'slug',
+    function ($name) {
+        $s = new Slugify();
+        return $s->slugify($name);
+    },
     array('is_safe' => array('html')
 ));
 $twig->addFilter($slug);
