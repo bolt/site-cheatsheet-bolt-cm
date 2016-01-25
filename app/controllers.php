3b492f656f485d8905a68356ea1768b74486c22a
Bob den Otter <bob@twokings.nl>
2016-01-25 20:54:15 +0100

    Making a basic Silex app.

---
+++ app/controllers.php
@@ -0,0 +1,60 @@
+<?php
+
+use Silex\Api\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class BaseControllers implements ControllerProviderInterface
+{
+
+    public function connect(Silex\Application $app)
+    {
+        /** @var $ctr \Silex\ControllerCollection */
+        $ctr = $app['controllers_factory'];
+
+        $ctr->get("/", array($this, 'home'))
+            ->bind('home');
+
+        $ctr->before(array($this, 'before'));
+
+        return $ctr;
+
+    }
+
+    public function home(Silex\Application $app)
+    {
+        $yaml = new Symfony\Component\Yaml\Parser();
+        $cheatsheet = $yaml->parse(file_get_contents(__DIR__ . '/cheatsheet.yml'));
+
+        return $app['twig']->render('index.twig', ['cheatsheet' => $cheatsheet]);
+    }
+
+    /**
+     * Middleware function to do some tasks that should be done for all requests.
+     */
+    public function before(Request $request, Silex\Application $app)
+    {
+        $app['twig']->addGlobal('config', $app['config']);
+    }
+
+    public function error(\Exception $e, Request $request, $code)
+    {
+        if ($app['debug']) {
+            return;
+        }
+
+        // 404.html, or 40x.html, or 4xx.html, or error.html
+        $templates = [
+            'errors/'.$code.'.html.twig',
+            'errors/'.substr($code, 0, 2).'x.html.twig',
+            'errors/'.substr($code, 0, 1).'xx.html.twig',
+            'errors/default.html.twig',
+        ];
+
+        return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+    }
+
+}
